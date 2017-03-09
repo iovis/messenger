904069a5a72cee11ae81c7399db91e7bcc5e152c
David Marchante <davidmarchan@gmail.com>
2017-03-10 00:09:37 +0100

    Added support for embed YouTube

--- main.js
+++ main.js
@@ -17,27 +17,34 @@ function onClosed () {
 function onNewWindow (event, newURL, frameName, disposition, options) {
   const urlObject = url.parse(newURL, true)
 
-  // Any external link will be sent to the default browser
-  if (urlObject.hostname !== 'l.messenger.com') {
-    event.preventDefault()
-    return shell.openExternal(urlObject.href)
-  }
   // Change width and height to something better
   options.width = 1280
   options.height = 800
 
   // Messenger has some interesting views for some pages
-  const targetURL = url.parse(urlObject.query.u)
+  if (urlObject.hostname === 'l.messenger.com') {
+    const targetURL = url.parse(urlObject.query.u)
 
-  // Allowed targets
-  if (targetURL.hostname === 'media.giphy.com' ||         // Giphy
+    // Allowed targets
+    if (targetURL.hostname === 'media.giphy.com' ||       // Giphy
       targetURL.href.match(/google\.\w{1,3}\/maps\//i) || // Google Maps
       targetURL.href.match(/goo.gl\/maps\//i)             // Google Maps
-  ) {
+    ) {
+      return true
+    }
+  // Show YouTube video in an embedded window
+  } else if (urlObject.hostname === 'www.youtube.com') {
+    event.preventDefault()
+
+    // Create a new window for the video
+    const win = new BrowserWindow(options)
+    win.loadURL(`https://www.youtube.com/embed/${urlObject.query.v}`)
+    win.on('closed', onClosed)
+
     return true
   }
 
-  // If it's not known, don't show it
+  // Any external link will be sent to the default browser
   event.preventDefault()
   return shell.openExternal(urlObject.href)
 }
